2161979532513ac2dc6329e220a84a22bdf4c8c1
Jonathan Bell <1707639+jonathanbell@users.noreply.github.com>
2023-04-03 17:16:49 -0700

    Ensure Astro files are linted by ESLint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,12 +8,29 @@ module.exports = {
 	},
 	extends: [
 		"standard",
+		"plugin:astro/recommended",
 		"plugin:vue/vue3-recommended",
 	],
 	plugins: ["jest"],
 	env: {
 		"jest/globals": true,
 	},
+	overrides: [
+		{
+			// Define the configuration for `.astro` file.
+			files: ["*.astro"],
+			// Allows Astro components to be parsed.
+			parser: "astro-eslint-parser",
+			parserOptions: {
+				parser: "@typescript-eslint/parser",
+				extraFileExtensions: [".astro"],
+			},
+			rules: {
+				// override/add rules settings here, such as:
+				// "astro/no-set-html-directive": "error"
+			},
+		},
+	],
 	rules: {
 		"no-tabs": ["error", { allowIndentationTabs: true }],
 		semi: ["error", "always"],
