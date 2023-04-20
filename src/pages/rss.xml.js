f3381e0ddb26cc65278376c6870c736e45b18470
Jonathan Bell <jbell@benevity.com>
2023-04-20 14:23:30 -0700

    Avoid adding draft items to RSS feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,9 +2,10 @@ import rss from "@astrojs/rss";
 import sanitizeHtml from "sanitize-html";
 
 export async function get(context) {
-	const postImportResult = import.meta.glob("./blog/*.md", { eager: true })
+	const postImportResult = import.meta.glob("./blog/*.md", { eager: true });
+
+	const posts = Object.values(postImportResult)
 		.filter((post) => !post.frontmatter.title.includes("DRAFT"));
-	const posts = Object.values(postImportResult);
 
 	const items = posts.map((post) => {
 		const p = {
