18937427ff8679acd88b1b1f4c54874ba9908ba8
jonathan-bell <jonathan.bell@workday.com>
2024-12-19 21:42:37 -0800

    ci: run prettier over codebase

--- tests/linkspage.spec.ts
+++ tests/linkspage.spec.ts
@@ -2,13 +2,13 @@ import { test, expect } from "@playwright/test";
 
 test("the links section is on the page", async ({ page }) => {
   await page.goto("/links");
-  const linksSection = await page.$("[data-testid=\"links-section\"]");
+  const linksSection = await page.$('[data-testid="links-section"]');
   expect(linksSection).toBeTruthy();
 });
 
 test("the links are external", async ({ page }) => {
   await page.goto("/links");
-  const linksSection = await page.$("[data-testid=\"links-section\"]");
+  const linksSection = await page.$('[data-testid="links-section"]');
   const bigOlButtons = await linksSection?.$$("a");
   expect(bigOlButtons?.length).toBeGreaterThan(6);
 
@@ -17,7 +17,10 @@ test("the links are external", async ({ page }) => {
       const href = await button.getAttribute("href");
       expect(href).toBeTruthy();
 
-      if (href === "https://log.jonathanbell.ca/" || href === "mailto:jonathanbell.ca+site@gmail.com") {
+      if (
+        href === "https://log.jonathanbell.ca/" ||
+        href === "mailto:jonathanbell.ca+site@gmail.com"
+      ) {
         continue;
       }
 
